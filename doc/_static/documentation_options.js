eb1eba098f1dfd58a13c09099021af5b9004d9b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-05 06:22:54 +0000

    Deploy open-power/skiboot to github.com/open-power/skiboot.git:gh-pages

---
+++ doc/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v6.6-22-g5b1d49d',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
